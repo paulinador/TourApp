113b79a5d2563fb40f07c849b3016637727588ba
Paulina <paulina.dorozko@gmail.com>
2024-04-05 10:33:55 +0200

    Add initial files

---
+++ TourApp/TourApp/TourAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourAppApp.swift
+//  TourApp
+//
+//  Created by Paulina Dorożko on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct TourAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
