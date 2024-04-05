8ea88be191448cf333ced142aa1dff625b78ccaf
Paulina <paulina.dorozko@gmail.com>
2024-04-05 11:05:25 +0200

    Add Details Repository

---
+++ TourApp/TourApp/Networking/DetailsRepository.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsRepository.swift
+//  TourApp
+//
+//  Created by Paulina Dorożko on 05/04/2024.
+//
+
+import Foundation
+
+protocol DetailsRepositoryProtocol {
+    func getObjectDetails(xid: String) async throws -> DetailsResponse
+}
+
+class DetailsRepository: DetailsRepositoryProtocol {
+    private let apiClient: APIClient
+    
+    init(apiClient: APIClient) {
+        self.apiClient = apiClient
+    }
+    
+    func getObjectDetails(xid: String) async throws -> DetailsResponse {
+        do {
+            let detail = try await apiClient.perform(request: DetailsRequest(xid: xid))
+            return detail
+        } catch {
+            throw error
+        }
+    }
+}
